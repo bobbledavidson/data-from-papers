6510a8ae18a62a591b9ee77adb13c5476346f4a8
bobbledavidson <bobbledavidson@yahoo.co.uk>
2015-10-25 12:39:31 -0600

    beginning

--- dfp_django/data_registry/views.py
+++ dfp_django/data_registry/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from django.views.generic.list import ListView
+from django.utils import timezone
+
+from data_registry.models import Papers
+
+def index(request):
+    return HttpResponse("data regitry index!")
+
+
+class PaperListView(ListView):
+
+    model = Papers
+
+    def get_context_data(self, **kwargs):
+        context = super(PaperListView, self).get_context_data(**kwargs)
+        #context['now'] = timezone.now()
+        return context
+
+from django.views.generic import list_detail
+from django.shortcuts import get_object_or_404
+
+def PaperDetailView(request,paper_id):
+
+    paper = get_object_or_404(Papers, pk=paper_id)
+
+    return list_detail.object_detail(
+
+        request,
+        queryset = Papers.objects.all(),
+        object_id = author_id,
+    )
+
--- dfp_django/dfp/urls.py
+++ dfp_django/dfp/urls.py
@@ -1,10 +1,21 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+
+from django.views.generic import TemplateView
+
+from data_registry import views
+from data_registry.views import PaperListView
+from data_registry.views import PaperDetailView
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'dfp.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    #url(r'^dfp/',include('dfp.urls')),
+    url(r'^$',TemplateView.as_view(template_name="index.html")),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^papers/(?P<paper_id>[0-9]+)/$',PaperDetailView,name='paper-detail'),
+    url(r'^papers/',PaperListView.as_view(),name='paper-list'),
+
 )
